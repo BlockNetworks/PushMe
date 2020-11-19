ea24d9f2db115ae3a39ac378825298a17a9757a0
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-11-18 18:10:15 -0800

    Update Loader.php

--- src/PushMe/Loader.php
+++ src/PushMe/Loader.php
@@ -1,7 +1,8 @@
 <?php
+
 namespace PushMe;
 
-use pocketmine\plugin\PluginBase as Plugin;
+use pocketmine\plugin\PluginBase;
 
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerMoveEvent;
@@ -16,38 +17,44 @@ use pocketmine\block\Block;
 
 use pocketmine\utils\Config;
 
-class Loader extends Plugin implements Listener {
-    public function onEnable() {
+class Loader extends PluginBase implements Listener
+{
+
+    public function onEnable()
+    {
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
         $this->getServer()->getLogger()->info("PushMe Enabled");
-        
-        $this->cfg = new Config($this->getDataFolder() ."config.yml", Config::YAML, [
-	    "ID" => 152,
-    	"NoPerm_Msg" => "no permission",
-        "Power" => 0.5
-    ]);
 
+        $this->cfg = new Config($this->getDataFolder() . "config.yml", Config::YAML, [
+            "ID" => 152,
+            "NoPerm_Msg" => "no permission",
+            "Power" => 0.5
+        ]);
     }
 
-    public function onPlayerMove(PlayerMoveEvent $event) {
+    public function onPlayerMove(PlayerMoveEvent $event)
+    {
         $player = $event->getPlayer();
         $direction = $player->getDirectionVector();
         $x = $direction->getX();
         $z = $direction->getZ();
         $block = $event->getBlock();
-        if($block->getSide(Vector3::SIDE_DOWN)->getId() === $this->cfg->get("ID"))
-            if($player->hasPermission("p.use")) {
-                for($i = 1; $i <= 1000; $i++) {
+        if ($block->getSide(Vector3::SIDE_DOWN)->getId() === $this->cfg->get("ID")) {
+            if ($player->hasPermission("p.use")) {
+                for ($i = 1; $i <= 1000; $i++) {
                     $player->knockBack($player, 0, $x, $z, $this->cfg->get("Power"));
                 }
                 $player->getLevel()->addSound(new FizzSound(new Vector3($player->getX(), $player->getY(), $player->getZ())));
             } else {
                 $player->sendTip($this->cfg->get("NoPerm_Msg"));
                 $player->getLevel()->addSound(new ClickSound(new Vector3($player->getX(), $player->getY(), $player->getZ())));
+            }
 		}
 	}
 
-    public function onDisable() {
+    public function onDisable()
+    {
         $this->getServer()->getLogger()->info("PushME Disabled");
     }
+
 }
